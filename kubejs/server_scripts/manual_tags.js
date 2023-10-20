6a11b1f044a0c640b4c179189cac8a27e01ce130
LeeHuangChenCS <37783562+LeeHuangChenCS@users.noreply.github.com>
2023-10-19 19:34:58 -0500

    fish tacklebox tutorial quest

---
+++ kubejs/server_scripts/manual_tags.js
@@ -0,0 +1,22 @@
+ServerEvents.tags('item', event => {
+  event.add('forge:aqua_rods', [
+    "aquaculture:diamond_fishing_rod",
+    "aquaculture:gold_fishing_rod",
+    "aquaculture:iron_fishing_rod",
+    "aquaculture:neptunium_fishing_rod"
+  ])
+
+  event.add('forge:raw_fishes', 'aquaculture:fish_fillet_raw')
+  event.add('forge:fishes', 'aquaculture:fish_fillet_raw')
+  event.add('forge:cooked_fishes', 'aquaculture:fish_fillet_cooked')
+  event.add('forge:fishes', 'aquaculture:fish_fillet_cooked')
+
+  event.remove('artifacts:artifacts', ['artifacts:villager_hat'])
+
+  event.add('forge:waystones_and_obsidian', [
+    "waystones:mossy_waystone",
+    "waystones:sandy_waystone",
+    "waystones:waystone",
+    "minecraft:obsidian"
+  ])
+})
