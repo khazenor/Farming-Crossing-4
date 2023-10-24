1df29e3aaf9860676d2aa1578dc09d04076c79b2
LeeHuangChenCS <37783562+LeeHuangChenCS@users.noreply.github.com>
2023-10-23 21:34:39 -0500

    add food sales

--- $notes/scriptGenerator/exportScripts.py
+++ $notes/scriptGenerator/exportScripts.py
@@ -2,9 +2,11 @@ from src import furnitureScripts
 from src import buildingMaterialsScripts
 from src import marketShopGen
 from src import furnitureCutting
-from src import bountifulEntries
+from src import foodVillagerSales
+from src import fishCraftingTableSell
 if __name__ == "__main__":
-	bountifulEntries.generateBountifulEntries()
+	fishCraftingTableSell.generateFishCraftingSellRecipes()
+	foodVillagerSales.generateSales()
 	furnitureCutting.generateFurnitureCuttingRecipes()
 	marketShopGen.generateMarketShops()
 	buildingMaterialsScripts.exportScripts()
---
+++ $notes/scriptGenerator/src/foodVillagerSales.py
@@ -0,0 +1,53 @@
+from input import foodVillagerTrades
+from lib import kubejs
+from src import const
+import os
+
+professionName = "villagersplus:horticulturist"
+tradeExperience = 25
+priceMultiplier = 0.035
+numberToSell = 8
+
+# priceMod to frequency
+priceModFreq = {
+	-1: 1,
+	0: 4,
+	1: 2,
+	4: 1
+}
+
+def generateSales():
+	trades = ""
+	tradeEntries = foodVillagerTrades.villagerTrades
+	for tradeLevel in tradeEntries:
+		itemIds = tradeEntries[tradeLevel]
+		for itemId in itemIds:
+			basePrice = tradeLevel
+			if tradeLevel == 1:
+				trades += kubejs.villagerTradeWCallback(
+					f'{numberToSell}x {itemId}',
+					f"{basePrice}x kubejs:miles_ticket",
+					professionName,
+					tradeLevel,
+					tradeExperience,
+					priceMultiplier
+				)
+			else:
+				for priceMod in priceModFreq:
+					price = basePrice + priceMod
+					if price > 0:
+						freq = priceModFreq[priceMod]
+						for i in range(freq):
+							trades += kubejs.villagerTradeWCallback(
+								f'{numberToSell}x {itemId}',
+								f"{price}x kubejs:miles_ticket",
+								professionName,
+								tradeLevel,
+								tradeExperience,
+								priceMultiplier
+							)
+
+	with(open(os.path.join(const.serverScripts(), "food_villager_sales.js"), "w")) as f:
+		f.write(kubejs.villagerTradesContent(trades))
+
+
