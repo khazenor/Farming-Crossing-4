72e73f8b5a60b54c88392c30d77cbf291637288c
LeeHuangChenCS <37783562+LeeHuangChenCS@users.noreply.github.com>
2023-11-03 00:45:17 -0500

    market quest

---
+++ kubejs/server_scripts/manual_modded_recipes.js
@@ -0,0 +1,36 @@
+ServerEvents.recipes(event => {
+  
+  event.remove({ output: 'farmingforblockheads:market' })
+  event.shapeless(
+    Item.of('farmingforblockheads:market', 1),
+    [
+      'kubejs:miles_ticket', 'kubejs:miles_ticket',
+      'kubejs:miles_ticket', 'kubejs:miles_ticket'
+    ]
+  )
+
+  event.shaped('refinedstorage:1k_storage_disk', [
+    'X',
+    'X'
+  ], {
+    X: 'kubejs:1k_storage_disk_ticket'
+  })
+  event.shaped('refinedstorage:4k_storage_disk', [
+    'X',
+    'X'
+  ], {
+    X: 'kubejs:4k_storage_disk_ticket'
+  })
+  event.shaped('refinedstorage:16k_storage_disk', [
+    'X',
+    'X'
+  ], {
+    X: 'kubejs:16k_storage_disk_ticket'
+  })
+  event.shaped('refinedstorage:64k_storage_disk', [
+    'X',
+    'X'
+  ], {
+    X: 'kubejs:64k_storage_disk_ticket'
+  })
+})
