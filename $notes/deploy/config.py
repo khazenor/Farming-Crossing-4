7dc479ea828b02196aac926242cd56bf9386eef2
LeeHuangChenCS <37783562+LeeHuangChenCS@users.noreply.github.com>
2023-11-05 15:38:57 -0600

    update world gen

--- $notes/deploy/config.py
+++ $notes/deploy/config.py
@@ -15,7 +15,7 @@ def questUpdateServer():
 
 def servers():
 	return [
-		'D:\\Lee\\Minecraft\\MultiMC\\instances\\Farming Crossing 4\\.minecraft\\$notes\\server\\Farming Crossing 4 Server'
+		'D:\\Lee\\Minecraft\\MultiMC\\instances\\Farming Crossing 4\\.minecraft\\$notes\\server\\Farming Crossing 4 Server',
 		'D:\\Lee\\Minecraft\\MultiMC\\instances\\Farming Crossing 4\\.minecraft\\$notes\\server\\TestingServer'
 	]
 
