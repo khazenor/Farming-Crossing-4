192da846567954f05a7d24480ce8f66a6d469b60
LeeHuangChenCS <37783562+LeeHuangChenCS@users.noreply.github.com>
2023-11-06 20:45:42 -0600

    add global game rules

---
+++ kubejs/server_scripts/global_game_rules.js
@@ -0,0 +1,6 @@
+ServerEvents.loaded(e => {
+  e.server.gameRules.set("doInsomnia", false)
+  e.server.gameRules.set("doPatrolSpawning", false)
+  e.server.gameRules.set("disableRaids", true)
+  e.server.gameRules.set("playersSleepingPercentage", 0)
+})
