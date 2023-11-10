81aef81e35b5dc723e19aae176212d4642a6cee5
LeeHuangChenCS <37783562+LeeHuangChenCS@users.noreply.github.com>
2023-11-10 00:02:07 -0600

    add InventoryProfilesNext

--- $notes/deploy/config.py
+++ $notes/deploy/config.py
@@ -97,5 +97,6 @@ def clientSideModNames():
 		'BetterAdvancements',
 		'controllable',
 		'entity_model_features_forge_1.20.1',
-		'entity_texture_features_forge_1.20.1'
+		'entity_texture_features_forge_1.20.1',
+		'InventoryProfilesNext'
 	]
