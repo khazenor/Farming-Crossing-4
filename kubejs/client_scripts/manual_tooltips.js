0ede79395e7e5c3fa2eb61d003cdd8653355bb39
LeeHuangChenCS <37783562+LeeHuangChenCS@users.noreply.github.com>
2023-11-09 23:49:34 -0600

    add recipe unlock tooltips and price up planes

---
+++ kubejs/client_scripts/manual_tooltips.js
@@ -0,0 +1,30 @@
+ItemEvents.tooltip(event => {
+  event.add(
+    [
+      "aquaculture:iron_hook",
+      "aquaculture:gold_hook",
+      "aquaculture:diamond_hook",
+      "aquaculture:light_hook",
+      "aquaculture:heavy_hook",
+      "aquaculture:double_hook",
+      "aquaculture:redstone_hook",
+      "aquaculture:note_hook",
+      "aquaculture:nether_star_hook",
+      "refinedstorage:crafting_grid",
+      "refinedstorage:1k_storage_disk",
+      "refinedstorage:4k_storage_disk",
+      "refinedstorage:16k_storage_disk",
+      "refinedstorage:64k_storage_disk",
+      "bagofyurting:bag_of_yurting",
+      "immersive_aircraft:airship",
+      "immersive_aircraft:cargo_airship",
+      "immersive_aircraft:biplane",
+      "immersive_aircraft:gyrodyne",
+      "immersive_aircraft:quadrocopter"
+    ],
+    [
+      "Unlock the crafting recipe for this item",
+      "by completing quests in the quest book."
+    ])
+
+})
