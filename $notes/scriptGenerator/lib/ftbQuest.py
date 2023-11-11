c120a7dee632889abceb3d57278920011126c46c
LeeHuangChenCS <37783562+LeeHuangChenCS@users.noreply.github.com>
2023-11-10 19:31:31 -0600

    collection achievement quests

---
+++ $notes/scriptGenerator/lib/ftbQuest.py
@@ -0,0 +1,71 @@
+import random
+parentStr = "0123456789ABCDEF"
+stringLength = 15
+
+def randomId():
+	idStr = random.choice("01234567")
+	for idx in range(stringLength):
+		idStr += random.choice(parentStr)
+	return idStr
+
+def questFileContent(icon, filename, title, questContent):
+	outStr = ""
+	outStr += f'{{\n'
+	outStr += f'	default_hide_dependency_lines: false\n'
+	outStr += f'	default_quest_shape: ""\n'
+	outStr += f'	filename: "{filename}"\n'
+	outStr += f'	group: ""\n'
+	outStr += f'	icon: "{icon}"\n'
+	outStr += f'	id: "{randomId()}"\n'
+	outStr += f'	order_index: 0\n'
+	outStr += f'	quest_links: []\n'
+	outStr += f'	quests: [\n'
+	outStr += questContent
+	outStr += f'	]\n'
+	outStr += f'	title: "{title}"'
+	outStr += f'}}\n'
+	return outStr
+
+def mileageRewardQuestContent(icon, numTickets, dependents, numRequired=0, title=None, x=0, y=0, lastQuestItem=None):
+	outStr = ""
+	outStr += f'{{\n'
+	outStr += f'	dependencies: [\n'
+	outStr += arrToStringContent(dependents, 4)
+	outStr += f'	]\n'
+	outStr += f'	icon: "{icon}"\n'
+	outStr += f'	id: "{randomId()}"\n'
+	if numRequired:
+		outStr += f'			min_required_dependencies: {numRequired}'
+	outStr += f'	rewards: [\n'
+	if lastQuestItem:
+		outStr += f'		{{\n'
+		outStr += f'			id: "{randomId()}"\n'
+		outStr += f'			item: "{lastQuestItem}"\n'
+		outStr += f'			type: "item"\n'
+		outStr += f'		}}\n'
+	outStr += f'		{{\n'
+	outStr += f'			count: {numTickets}\n'
+	outStr += f'			id: "{randomId()}"\n'
+	outStr += f'			item: "kubejs:miles_ticket"\n'
+	outStr += f'			type: "item"\n'
+	outStr += f'		}}\n'
+	outStr += f'	]\n'
+	outStr += f'	tasks: [{{\n'
+	outStr += f'		id: "{randomId()}"\n'
+	outStr += f'		type: "item"\n'
+	outStr += f'	}}]\n'
+	if title:
+		outStr += f'	title: "{title}"\n'
+	outStr += f'	x: {float(x)}d\n'
+	outStr += f'	y: {float(y)}d\n'
+	outStr += f'}}\n'
+
+	return outStr
+
+def arrToStringContent(array, tabs=0):
+	outStr = ""
+	for item in array:
+		for i in range(tabs):
+			outStr += "\t"
+		outStr += f'"{item}"\n'
+	return outStr
---
+++ $notes/scriptGenerator/mileageRewardProgramQuestGen.py
@@ -0,0 +1,85 @@
+from lib import ftbQuest
+import random
+from input import mileageRewardCollectionQuests as mrc
+import math
+from src import const
+import os
+
+def genMileageRewardProgramQuests():
+	filename = "collection_achievements"
+	random.seed(123)
+	fileDir = os.path.join(const.ftbChapters(), f"{filename}.snbt")
+	with open(fileDir, "w") as f:
+		f.write(ftbQuest.questFileContent(
+			"kubejs:miles_ticket",
+			filename,
+			"Collection Achievements",
+			getQuestContent(),
+		))
+
+def getQuestContent():
+	questContent = ""
+	numRequiredStarts = [2, 5]
+	startIncrement = 5
+	incrementIncreaseRate = 5
+
+	for y, collection in enumerate(mrc.collections):
+		numRequired = numRequiredStarts[0]
+		finalTicketValue = collection[mrc.finalTicketValueKey]
+		collectionQuestIds = collection[mrc.questIdsKey]
+		totalNumQuests = len(collectionQuestIds)
+		increment = startIncrement
+		x = 0
+		print(collection[mrc.nameKey])
+		while numRequired + increment < totalNumQuests:
+			if x == 0:
+				numUnpaidQuests = numRequired
+			elif x < len(numRequiredStarts):
+				numRequired = numRequiredStarts[x]
+				numUnpaidQuests = numRequiredStarts[x] - numRequiredStarts[x - 1]
+			ticketPerQuest = float(numRequired)/float(totalNumQuests) * finalTicketValue
+			ticketValue = math.ceil(ticketPerQuest * numUnpaidQuests)
+
+			# title strings
+			action = collection[mrc.actionKey]
+			itemName = collection[mrc.nameKey] + collection[mrc.pluralKey]
+
+			questContent += ftbQuest.mileageRewardQuestContent(
+				collection[mrc.iconKey],
+				ticketValue,
+				collectionQuestIds,
+				numRequired=numRequired,
+				title=f'{action} {numRequired} Unique {itemName}!',
+				x=x,
+				y=y
+			)
+			print(numRequired)
+
+			# end values
+			if numRequired + increment + increment < totalNumQuests:
+				numUnpaidQuests = increment
+			else:
+				numUnpaidQuests = totalNumQuests - numRequired
+
+			# update values outside of the first few quests
+			if x >= len(numRequiredStarts) - 1:
+				numRequired += increment
+				increment += incrementIncreaseRate
+
+			x += 1
+
+		# final completion quest
+		questContent += ftbQuest.mileageRewardQuestContent(
+			collection[mrc.iconKey],
+			numUnpaidQuests * finalTicketValue,
+			collectionQuestIds,
+			title=f'{collection[mrc.collectionKey]} Completion!',
+			x=x,
+			y=y,
+			lastQuestItem=collection[mrc.lastQuestItemKey]
+		)
+		print(totalNumQuests)
+	return questContent
+
+if __name__ == "__main__":
+	genMileageRewardProgramQuests()
