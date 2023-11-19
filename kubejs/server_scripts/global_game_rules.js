bc1a2ad857076237dec75be6fec35ab926d32802
LeeHuangChenCS <37783562+LeeHuangChenCS@users.noreply.github.com>
2023-11-19 16:37:19 -0600

    jess: disable wandering traders

--- kubejs/server_scripts/global_game_rules.js
+++ kubejs/server_scripts/global_game_rules.js
@@ -3,4 +3,5 @@ ServerEvents.loaded(e => {
   e.server.gameRules.set("doPatrolSpawning", false)
   e.server.gameRules.set("disableRaids", true)
   e.server.gameRules.set("playersSleepingPercentage", 0)
+  e.server.gameRules.set("doTraderSpawning", false)
 })
