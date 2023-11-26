3092f9a7b841f0aeac75997f0bbaefff8f5cc27f
LeeHuangChenCS <37783562+LeeHuangChenCS@users.noreply.github.com>
2023-11-26 10:29:55 -0600

    add deploy prod script

---
+++ $notes/deploy/DeployProd.py
@@ -0,0 +1,14 @@
+from src import paths
+from src import deploy
+from src import log
+
+if __name__ == "__main__":
+	log.init()
+	paths.configSrc = "D:\\Lee\\Minecraft\\Twitch\\Instances\\Farming Crossing 4"
+	paths.modsSrc = paths.configSrc
+
+	paths.otherInsts = [
+		'D:\\Lee\\Minecraft\\MultiMC\\instances\\Farming Crossing 4 Prod\\.minecraft'
+	]
+
+	deploy.deployModpack()
