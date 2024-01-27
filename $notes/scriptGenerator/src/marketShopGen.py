a9308f4809d175e28677117141d8a44a76a36686
LeeHuangChenCS <37783562+LeeHuangChenCS@users.noreply.github.com>
2024-01-27 16:30:33 -0600

    marketShopRefactor

--- $notes/scriptGenerator/src/marketShopGen.py
+++ $notes/scriptGenerator/src/marketShopGen.py
@@ -6,67 +6,8 @@ from src import const
 import json
 import os
 
-numMeat = 8
-meatPrice = 2
-numSeed = 4
-seedPrice = 32
-numSapling = 2
-saplingPrice = 32
-flowerPrice = 32
-spawnEggPrice = 64
-
 def generateMarketShops():
-	meatShop = generateMarketShop(
-		marketShop.meats,
-		numMeat,
-		meatPrice,
-		'meats',
-		"Ethically Sourced Meats",
-		"minecraft:rabbit",
-	)
-	meatShop = addEntries(meatShop, marketShop.more_meats, 'meats')
-	json.dump(meatShop, open(os.path.join(const.farmingForBlockheads(), 'meats.json'), 'w'), indent=2)
-
-	generateMarketShopAndWriteFile(
-		marketShop.seeds,
-		numSeed,
-		seedPrice,
-		'seed',
-		"Seed Bank",
-		"minecraft:wheat_seeds",
-		os.path.join(const.farmingForBlockheads(), 'seeds.json')
-	)
-
-	generateMarketShopAndWriteFile(
-		marketShop.saplings,
-		numSeed,
-		saplingPrice,
-		'sapling',
-		"The Nursery",
-		"minecraft:oak_sapling",
-		os.path.join(const.farmingForBlockheads(), 'saplings.json')
-	)
-
-	generateMarketShopAndWriteFile(
-		marketShop.flowers,
-		numSeed,
-		flowerPrice,
-		'flowers',
-		"Flower Shop",
-		"minecraft:poppy",
-		os.path.join(const.farmingForBlockheads(), 'flowers.json')
-	)
-
-	generateMarketShopAndWriteFile(
-		marketShop.spawn_eggs,
-		1,
-		spawnEggPrice,
-		'spawneggs',
-		"Spawn Eggs Shop",
-		"alexsmobs:spawn_egg_anaconda",
-		os.path.join(const.farmingForBlockheads(), 'spawn_eggs.json')
-	)
-
+	farmingForBlockheads.genCategoryStores(marketShop.categories)
 	generateShopFromEnchantments(
 		marketShop.enchantments,
 		'enchantments',
@@ -74,43 +15,6 @@ def generateMarketShops():
 		'minecraft:enchanted_book',
 		os.path.join(const.farmingForBlockheads(), 'enchantments.json')
 	)
-
-def generateMarketShop(items, itemCount, price, category, categoryName, categoryItem):
-	entries = []
-	for item in items:
-		entries.append(farmingForBlockheads.entry(
-			item,
-			itemCount,
-			const.priceItem,
-			price,
-			category
-		))
-
-	shop = farmingForBlockheads.categoryStore(
-		entries,
-		category,
-		categoryName,
-		categoryItem
-	)
-	return shop
-
-def addEntries(shop, entries, category):
-	outShop = shop
-	for entry in entries:
-		outShop['customEntries'].append(farmingForBlockheads.entry(
-			entry[1],
-			entry[0],
-			const.priceItem,
-			entry[2],
-			category
-		))
-	return outShop
-
-
-def generateMarketShopAndWriteFile(items, itemCount, price, category, categoryName, categoryItem, fileLocation):
-	shop = generateMarketShop(items, itemCount, price, category, categoryName, categoryItem)
-	json.dump(shop, open(fileLocation, 'w'), indent=2)
-
 
 def generateShopFromEnchantments(enchantments, category, categoryName, categoryItem, fileLocation):
 	entries = []
