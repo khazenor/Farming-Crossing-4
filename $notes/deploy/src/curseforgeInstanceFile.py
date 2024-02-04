c1cebb85d7a74bf35f110c7d89b2039949deaf54
LeeHuangChenCS <37783562+LeeHuangChenCS@users.noreply.github.com>
2024-02-04 09:31:07 -0600

    trying ordered dict

--- $notes/deploy/src/curseforgeInstanceFile.py
+++ $notes/deploy/src/curseforgeInstanceFile.py
@@ -1,5 +1,5 @@
 from src import paths
-import shutil
+from collections import OrderedDict
 from src import log
 import json
 import os
@@ -9,5 +9,6 @@ def deployCurseforgeFile():
 		log.log(' # Deploy Curseforge Instance file')
 		instanceFileUrl = os.path.join(paths.modsSrc, 'minecraftinstance.json')
 		fileJson = json.load(open(instanceFileUrl, 'r', encoding="utf-8"))
+		fileJson = OrderedDict(fileJson)
 		copyUrl = os.path.join(paths.configSrc, 'curseforge_instance.json')
 		json.dump(fileJson, open(copyUrl, 'w', encoding="utf-8"), indent=2)
