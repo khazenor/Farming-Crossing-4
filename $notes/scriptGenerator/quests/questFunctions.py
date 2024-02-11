2a3f87c7baa5bb4a748d9e013c567ab25fe2ab2f
LeeHuangChenCS <37783562+LeeHuangChenCS@users.noreply.github.com>
2024-02-11 11:55:48 -0600

    move quest functions to it's own folder

--- $notes/scriptGenerator/quests/questFunctions.py
+++ $notes/scriptGenerator/quests/questFunctions.py
@@ -3,14 +3,18 @@ from lib import mcfunction
 from lib import commands
 from lib import stringCleaning
 
+functionParentName = 'fc_collection'
 def genQuestFunctions():
 	initFunctionContent = ''
+	mcfunction.clearFunctionFiles(functionParentName)
+
 	for questline in collectionQuestsInput.questlines:
 		totalCollectibles = 0
 		for questGroup in questline[collectionQuestsInput.questGroupsKey]:
 			subGroupName = questGroup[collectionQuestsInput.nameKey]
 			totalSubCollectibles = len(questGroup[collectionQuestsInput.tasksKey])
 			mcfunction.writeFunction(
+				functionParentName,
 				subGroupName,
 				commands.collectionNotification(
 					subGroupName,
@@ -24,6 +28,7 @@ def genQuestFunctions():
 		questlineName = questline[collectionQuestsInput.nameKey]
 		initFunctionContent += commands.initScoreBoard(questlineName)
 		mcfunction.writeFunction(
+			functionParentName,
 			questlineName,
 			commands.collectionNotification(
 				questline[collectionQuestsInput.collectionNotificationKey],
@@ -31,4 +36,4 @@ def genQuestFunctions():
 				totalCollectibles
 			)
 		)
-		mcfunction.writeFunction('init_all_scores', initFunctionContent)
+		mcfunction.writeFunction(functionParentName, 'init_all_scores', initFunctionContent)
