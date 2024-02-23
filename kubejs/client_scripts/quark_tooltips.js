f5718df303fc789bfd97e0a82c3e24a3d458c275
khazenor <80185732+khazenor@users.noreply.github.com>
2024-02-23 06:20:11 -0600

    quark ticket support and tooltips

---
+++ kubejs/client_scripts/quark_tooltips.js
@@ -0,0 +1,42 @@
+ItemEvents.tooltip(event => {
+  event.add('minecraft:white_candle', ["Enchant Influences: Unbreaking"])
+  event.add('minecraft:orange_candle', ["Enchant Influences: Fire Protection"])
+  event.add('minecraft:magenta_candle', ["Enchant Influences: Knockback, Punch"])
+  event.add('minecraft:light_blue_candle', ["Enchant Influences: Feather Falling"])
+  event.add('minecraft:yellow_candle', ["Enchant Influences: Looting, Fortune, Luck of the Sea"])
+  event.add('minecraft:lime_candle', ["Enchant Influences: Blast Protection"])
+  event.add('minecraft:pink_candle', ["Enchant Influences: Silk Touch, Channeling"])
+  event.add('minecraft:gray_candle', ["Enchant Influences: Bane of Arhtropods"])
+  event.add('minecraft:light_gray_candle', ["Enchant Influences: Protection"])
+  event.add('minecraft:cyan_candle', ["Enchant Influences: Respiration, Loyalty, Infinity"])
+  event.add('minecraft:purple_candle', ["Enchant Influences: Sweeping Edge, Multishot"])
+  event.add('minecraft:blue_candle', ["Enchant Influences: Efficiency, Sharpness, Lure, Power, Impaling, Quick Charge"])
+  event.add('minecraft:brown_candle', ["Enchant Influences: Aqua Affinity, Depth Strider, Riptide"])
+  event.add('minecraft:green_candle', ["Enchant Influences: Thorns, Piercing"])
+  event.add('minecraft:red_candle', ["Enchant Influences: Fire Aspect, Flame"])
+  event.add('minecraft:black_candle', ["Enchant Influences: Smite, Projectile Protection"])
+
+  event.add([
+    "quark:black_corundum",
+    "quark:blue_corundum",
+    "quark:green_corundum",
+    "quark:indigo_corundum",
+    "quark:orange_corundum",
+    "quark:red_corundum",
+    "quark:violet_corundum",
+    "quark:white_corundum",
+    "quark:yellow_corundum",
+    "quark:black_corundum_cluster",
+    "quark:blue_corundum_cluster",
+    "quark:green_corundum_cluster",
+    "quark:indigo_corundum_cluster",
+    "quark:orange_corundum_cluster",
+    "quark:red_corundum_cluster",
+    "quark:violet_corundum_cluster",
+    "quark:white_corundum_cluster",
+    "quark:yellow_corundum_cluster"
+  ], [
+    "4 of any corundum can be crafted",
+    'to 1 Miles Ticket'
+  ])
+})
