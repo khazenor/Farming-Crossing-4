4c43e4fbbd46cb56846633301b2fbcbea4228c4b
khazenor <80185732+khazenor@users.noreply.github.com>
2024-03-28 18:08:56 -0500

    update tip hashing again

--- $notes/scriptGenerator/src/tips.py
+++ $notes/scriptGenerator/src/tips.py
@@ -3,15 +3,17 @@ from src import const
 import json
 import os
 from lib import util
+from lib import ftbQuest
 
 def genTips():
 	folder = os.path.join(const.assets(), "fc_tips", "tips")
 	util.removeFiles(folder)
+
 	for tip in tipsInput.tips:
 		json.dump({
 			"tip": {
 				"text": tip
 			}
 		}, open(
-			os.path.join(folder, f'tip_{hash(tip) % (10 ** 8)}.json'), "w"
+			os.path.join(folder, f'tip_{ftbQuest.randomId(tip)}.json'), "w"
 		))
