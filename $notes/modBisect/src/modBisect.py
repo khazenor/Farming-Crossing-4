b94df6cf7065cfb6b015725a506c68a534964c18
khazenor <80185732+khazenor@users.noreply.github.com>
2024-04-05 11:46:41 -0500

    update modbisect

--- $notes/modBisect/src/modBisect.py
+++ $notes/modBisect/src/modBisect.py
@@ -1,6 +1,6 @@
 from src import fileGroupings
 from src import config
-import json
+import random
 import os
 
 disabledStr = '.disabled'
@@ -15,32 +15,68 @@ def modBisect(excludeModIdList=[]):
 
 def test(goodGroups, mysteryGroups):
 	if len(mysteryGroups) > 1:
-		group1, group2 = splitList(mysteryGroups)
+		group1, group2 = splitListRandom(mysteryGroups)
 		# test group 1
-		print(f"Disable({len(group2)}): {group2}")
-		disableGroups(group2)
-		print(f"Good({len(goodGroups)}): {goodGroups}")
-		enableGroups(goodGroups)
-		print(f"Testing({len(group1)}): {group1}")
-		enableGroups(group1)
+		print(f"Group1: ")
+		disableAndEnableMods(group2, goodGroups, group1)
 
-		firstHalfGood = askIfFeatureIsWorking()
-		if firstHalfGood == 'quit':
-			return
+		firstHalfResponse = ''
+		while firstHalfResponse != 'yes' and firstHalfResponse != 'no':
+			firstHalfResponse = askIfFeatureIsWorking()
+			if firstHalfResponse == 'quit':
+				return
+			if firstHalfResponse == 'reroll':
+				test(goodGroups, mysteryGroups)
 
-		if firstHalfGood:
+		if firstHalfResponse == 'yes':
 			goodGroups += group1
-			test(goodGroups, group2)
+			# test group 2
+			print("Group2:")
+			disableAndEnableMods(group1, goodGroups, group2)
+			secondHalfResponse = ''
+			while secondHalfResponse != 'yes' and secondHalfResponse != 'no':
+				secondHalfResponse = askIfFeatureIsWorking()
+				if secondHalfResponse == 'quit':
+					return
+				if secondHalfResponse == 'reroll':
+					print('cannot resole on group 2')
+					secondHalfResponse = askIfFeatureIsWorking()
+
+			if secondHalfResponse == 'yes':
+				print("Can't find issue, both split parts are issue free, trying again with different split")
+				test(goodGroups, mysteryGroups)
+			else:
+				test(goodGroups, group2)
 		else:
 			test(goodGroups, group1)
 	else:
 		print(f"Found Problem Mod Group: {mysteryGroups[0]}")
 
+def disableAndEnableMods(disable, good, testing):
+	print(f"Disable({len(disable)}): {disable}")
+	disableGroups(disable)
+	print(f"Good({len(good)}): {good}")
+	enableGroups(good)
+	print(f"Testing({len(testing)}): {testing}")
+	enableGroups(testing)
 def askIfFeatureIsWorking():
-	response = input("Is the feature working? ").lower().strip()
-	if response.lower().strip() == 'quit':
+	response = input("Is the feature working? ")
+	if haveSubstrings(['stop', 'quit'], response):
 		return 'quit'
-	return 'y' in response
+	elif haveSubstrings(['reroll', 'roll'], response):
+		return 'reroll'
+	elif haveSubstrings(['yes'], response):
+		return 'yes'
+	elif haveSubstrings(['no'], response):
+		return 'no'
+	else:
+		return 'error'
+
+def haveSubstrings(subStrings, targetString):
+	for subString in subStrings:
+		if subString in targetString.lower().strip():
+			return True
+	return False
 
 def disableGroups(fileGroups):
 	for fileGroup in fileGroups:
@@ -71,8 +107,19 @@ def enableMods(filenames):
 				srcDir = modDir+disabledStr
 			os.rename(srcDir, modDir.replace(disabledStr, ""))
 
-def splitList(targetList):
-	return targetList[len(targetList)//2:], targetList[:len(targetList)//2]
+def splitListRandom(targetList):
+	group1 = []
+	group2 = []
+
+	for group in targetList:
+		if int(random.random() * 2):
+			group1.append(group)
+		else:
+			group2.append(group)
+	if len(group1) == 0 or len(group2) == 0:
+		return splitListRandom(targetList)
+
+	return group1, group2
 
 def listSubtract(mainList, subList):
 	differenceList = []
