f1ba214be37a3e8ae3a76223f4e38039e20b56e1
khazenor <80185732+khazenor@users.noreply.github.com>
2024-04-13 14:10:40 -0500

    season server support code cleanup

--- kubejs/server_scripts/seasons_support_server.js
+++ kubejs/server_scripts/seasons_support_server.js
@@ -8,6 +8,33 @@ const villagers = [
   'sam',
   'yukkie'
 ]
+
+ItemEvents.rightClicked('sereneseasons:calendar', event => {
+  updateVillagerAroundPlayer(
+    'Trades for nearby farming crossing villagers are now updated for',
+    event.player
+  )
+})
+
+PlayerEvents.loggedIn(event => {
+  updateVillagerAroundPlayer('The season is now', event.player)
+})
+
+const updateVillagerAroundPlayer = (noticeMsg, player) => {
+  const level = player.level
+  const season = getSeasonFromLevel(level)
+  player.tell(`${noticeMsg} ${season}`)
+  for (const villager of villagers) {
+    player.getServer().runCommandSilent(
+      `execute at @p run function fc_villagers:${villager}_update_trades_${season}`
+    )
+  }
+} 
+
+const getSeasonFromLevel = (level) => {
+  return seasonName(SeasonHelper.getSeasonState(level).getSeason())
+}
+
 const seasonName = (seasonObj) => {
   if (seasonObj == Season.SPRING) {
     return 'spring'
@@ -18,29 +45,4 @@ const seasonName = (seasonObj) => {
   } else {
     return 'winter'
   }
-} 
-let season
-
-const getSeasonFromLevel = (level) => {
-  return seasonName(SeasonHelper.getSeasonState(level).getSeason())
 }
-
-ItemEvents.rightClicked('sereneseasons:calendar', event => {
-  event.player.tell(`Trades for nearby farming crossing villagers are now updated for ${season}`)
-  updateVillagerAroundPlayer(event.player)
-})
-
-PlayerEvents.loggedIn(event => {
-  const level = event.player.level
-  season = getSeasonFromLevel(level)
-  event.player.tell(`The season is now ${season}`)
-  updateVillagerAroundPlayer(event.player)
-})
-
-const updateVillagerAroundPlayer = (player) => {
-  for (const villager of villagers) {
-    player.getServer().runCommandSilent(
-      `execute at @p run function fc_villagers:${villager}_update_trades_${season}`
-    )
-  }
-} 
