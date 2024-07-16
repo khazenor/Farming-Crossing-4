d4a80d43f49b0e5d61efec17a0c2d77676593714
khazenor <80185732+khazenor@users.noreply.github.com>
2024-07-16 16:48:18 -0500

    add creative controller crafting

--- kubejs/server_scripts/creative_upgrades.js
+++ kubejs/server_scripts/creative_upgrades.js
@@ -1,18 +1,16 @@
 ServerEvents.recipes(event => {
-  event.shapeless('refinedstorage:creative_portable_grid', [
-    'refinedstorage:portable_grid',
-    'kubejs:creative_upgrade'
-  ])
-  event.shapeless('refinedstorage:creative_wireless_fluid_grid', [
-    'refinedstorage:wireless_fluid_grid',
-    'kubejs:creative_upgrade'
-  ])
-  event.shapeless('refinedstorage:creative_wireless_grid', [
-    'refinedstorage:creative_wireless_grid',
-    'kubejs:creative_upgrade'
-  ])
-  event.shapeless('refinedstorageaddons:creative_wireless_crafting_grid', [
-    'refinedstorageaddons:wireless_crafting_grid',
-    'kubejs:creative_upgrade'
-  ])
+  const creativeUpgrades = {
+    'refinedstorage:creative_portable_grid': 'refinedstorage:portable_grid',
+    'refinedstorage:creative_wireless_fluid_grid': 'refinedstorage:wireless_fluid_grid',
+    'refinedstorage:creative_wireless_grid': 'refinedstorage:wireless_grid',
+    'refinedstorageaddons:creative_wireless_crafting_grid': 'refinedstorageaddons:wireless_crafting_grid',
+    'refinedstorage:creative_controller': 'refinedstorage:controller'
+  }
+
+  for (let creativeId in creativeUpgrades) {
+    event.shapeless(creativeId, [
+      creativeUpgrades[creativeId],
+      'kubejs:creative_upgrade'
+    ])
+  }
 })
